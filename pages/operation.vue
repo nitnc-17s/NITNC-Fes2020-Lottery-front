de5a264bb8ba50954aeece7ec9699bdc446fc8a7
akatsuki1910 <akatsukicirno@gmail.com>
2020-11-02 21:24:19 +0900

    approximately

--- pages/operation.vue
+++ pages/operation.vue
@@ -5,7 +5,28 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      num: 0,
+      content: "null",
+      name: "null",
+      huri: "null",
+      class: "null"
+    }
+  },
+  methods: {
+    setData() {
+      this.$store.commit('dataSet', [
+        this.num,
+        this.content,
+        this.name,
+        this.huri,
+        this.class
+      ])
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
--- pages/view/public.vue
+++ pages/view/public.vue
@@ -5,7 +5,28 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      num: 0,
+      content: "null",
+      name: "null",
+      huri: "null",
+      class: "null"
+    }
+  },
+  mounted () {
+    [
+      this.num,
+      this.content,
+      this.name,
+      this.huri,
+      this.class
+    ] = this.$store.getters.returnData
+  },
+  methods: {
+  }
+}
 </script>
 
 <style lang="scss" scoped>
